5ee543c3c28cb43db5342fbb1a32223a3bfad435
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-06-24 23:37:34 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframePl = document.querySelector('#vimeo-player');
+
+let currentTime;
+const player = new Player(iframePl);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  console.dir(event.seconds);
+  currentTime = event.seconds;
+  localStorage.setItem('currentTime', currentTime);
+}
+
+const sec = localStorage.getItem('currentTime');
+
+player.setCurrentTime(sec);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const STOREGE_KEY = 'feedback-form-state';
+let msgObj = {};
+
+feedbackForm.addEventListener('submit', hendlerSubmit);
+feedbackForm.addEventListener('input', throttle(hendlerInput, 500));
+
+fieldValue();
+
+function hendlerSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STOREGE_KEY);
+  msgObj = {};
+}
+function hendlerInput(evt) {
+  msgObj[evt.target.name] = evt.target.value;
+  localStorage.setItem(STOREGE_KEY, JSON.stringify(msgObj));
+}
+
+function fieldValue() {
+  if (localStorage.getItem(STOREGE_KEY)) {
+    const obj = JSON.parse(localStorage.getItem(STOREGE_KEY));
+    feedbackForm.elements.email.value = obj[feedbackForm.elements.email.name];
+    msgObj[feedbackForm.elements.email.name] =
+      obj[feedbackForm.elements.email.name];
+    feedbackForm.elements.message.value =
+      obj[feedbackForm.elements.message.name];
+    msgObj[feedbackForm.elements.message.name] =
+      obj[feedbackForm.elements.message.name];
+  }
+}
