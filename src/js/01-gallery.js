b735eed1f6cfe6516cc69b113a9c17becc1ff3b8
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-06-24 12:42:05 +0300

    dz1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const gallaryItems = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      title="${description}"
+    />
+  </a>`
+);
+galleryEl.insertAdjacentHTML('afterbegin', gallaryItems.join(''));
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
